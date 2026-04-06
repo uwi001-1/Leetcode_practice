cc65fa7a6a8677254153149c4a3d974c1e67e0c0
Kripa Hayanju <kripahayanju@gmail.com>
2026-04-06 13:08:10 -0500

    15

--- 8_Easy.cpp
+++ 8_Easy.cpp
@@ -1,28 +1,51 @@
 #include<iostream>
 using namespace std;
 
-// 371. Sum of Two Integers
+// 268. Missing Number
 
 /***************
-Given two integers a and b, return the sum of the two integers without using the operators + and -.
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
 Example 1:
-Input: a = 1, b = 2
-Output: 3
+Input: nums = [3,0,1]
+Output: 2
+    Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
 
 Example 2:
-Input: a = 2, b = 3
-Output: 5
+Input: nums = [0,1]
+Output: 2
+    Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 3:
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+    Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
 ***************/
 
+//With O(n2) --> it's too slow
 class Solution {
 public:
-    int getSum(int a, int b) {
-        while (b != 0) {
-            int carry = (a & b) << 1;
-            a = a ^ b;
-            b = carry;
+    int missingNumber(vector<int>& nums) {
+        // there is exaclty one number missing between 0 and nums.size()
+        bool found = false;
+
+        for(int i = 0; i <= nums.size(); i++)     //select a number like 0 to nums.size()
+        {
+            for(int j =0; j < nums.size(); j++)     //to check with the selected number and the number in the array
+            {
+                if(nums[j] == i)       //check, if found then break
+                {
+                    found = true;
+                    break;  
+                }
+                else
+                {
+                    found = false;
+                }
+            }
+            if(found == false)   //if not found through out the array, then that is the missing number
+                return {i};
         }
-    return a;
+        return {};
     }
 };
