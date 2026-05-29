86cf1e01e19c8376e0731dba9c7dc1733d248354
Kripa Hayanju <kripahayanju@gmail.com>
2026-05-28 21:14:17 -0500

    27

---
+++ 14_Medium.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+// 2469. Convert the Temperature
+
+/***************
+You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
+You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
+Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+
+Note that:
+Kelvin = Celsius + 273.15
+Fahrenheit = Celsius * 1.80 + 32.00
+ 
+Example 1:
+Input: celsius = 36.50
+Output: [309.65000,97.70000]
+    Explanation: Temperature at 36.50 Celsius converted in Kelvin is 309.65 and converted in Fahrenheit is 97.70.
+
+Example 2:
+Input: celsius = 122.11
+Output: [395.26000,251.79800]
+    Explanation: Temperature at 122.11 Celsius converted in Kelvin is 395.26 and converted in Fahrenheit is 251.798.
+***************/
+
+
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) 
+    {
+        double K = celsius + 273.15;
+        double F = celsius * 1.80 + 32.00;
+
+        return {K, F};
+    }
+};
