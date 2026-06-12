450a4f8877b08b491a464646dcb352b10f663966
Kripa Hayanju <kripahayanju@gmail.com>
2026-06-12 13:43:22 -0500

    57

--- 22_Easy.cpp
+++ 22_Easy.cpp
@@ -4,131 +4,67 @@ using namespace std;
 // 2535. Difference Between Element Sum and Digit Sum of an Array
 
 /***************
-Given a 0-indexed string s, permute s to get a new string t such that:
+You are given a positive integer array nums.
 
-All consonants remain in their original places. More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
-The vowels must be sorted in the nondecreasing order of their ASCII values. More formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
-Return the resulting string.
+The element sum is the sum of all the elements in nums.
+The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
+Return the absolute difference between the element sum and digit sum of nums.
 
-The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase. Consonants comprise all letters that are not vowels.
+Note that the absolute difference between two integers x and y is defined as |x - y|.
 
 Example 1:
-Input: s = "lEetcOde"
-Output: "lEOtcede"
-    Explanation: 'E', 'O', and 'e' are the vowels in s; 'l', 't', 'c', and 'd' are all consonants. The vowels are sorted according to their ASCII values, and the consonants remain in the same places.
+Input: nums = [1,15,6,3]
+Output: 9
+    Explanation: 
+    The element sum of nums is 1 + 15 + 6 + 3 = 25.
+    The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
+    The absolute difference between the element sum and digit sum is |25 - 16| = 9.
 
 Example 2:
-Input: s = "lYmpH"
-Output: "lYmpH"
-    Explanation: There are no vowels in s (all characters in s are consonants), so we return "lYmpH".
+Input: nums = [1,2,3,4]
+Output: 0
+    Explanation:
+    The element sum of nums is 1 + 2 + 3 + 4 = 10.
+    The digit sum of nums is 1 + 2 + 3 + 4 = 10.
+    The absolute difference between the element sum and digit sum is |10 - 10| = 0.
 ***************/
 
 
-// this is O(nlogn)
 class Solution {
 public:
-    string sortVowels(string s) 
+    int differenceOfSum(vector<int>& nums) 
     {
-        // make new character to push the vowels
-        vector<char> m;
+        // this data type as the element could be long 
+        long long ele = 0;
+        long long digit = 0; 
 
-        for(int i = 0; i < s.size(); i++)
+        for(int index : nums)
         {
-            // if the char is not vowel then continue
-            if(s[i] != 'A' 
-            && s[i] != 'E' 
-            && s[i] != 'O'
-            && s[i] != 'I' 
-            && s[i] != 'U'
-            && s[i] != 'a' 
-            && s[i] != 'e' 
-            && s[i] != 'i'
-            && s[i] != 'o' 
-            && s[i] != 'u')
-                continue;
-            
-            m.push_back(s[i]);
-        }
+            // add each element 
+            ele += index;
 
-        // sort the vowels based on their ascii values
-        std::sort(m.begin(), m.end());
-        
-        // for the vowels string
-        int num = 0;
-
-        for(int i = 0; i < s.size(); i++)
-        {
-            // if the char is not vowel then continue
-            if(s[i] != 'A' 
-            && s[i] != 'E' 
-            && s[i] != 'O'
-            && s[i] != 'I' 
-            && s[i] != 'U'
-            && s[i] != 'a' 
-            && s[i] != 'e' 
-            && s[i] != 'i'
-            && s[i] != 'o' 
-            && s[i] != 'u')
-                continue;
-            
-            // if it is a vowel replace it with the string in vowel
-            s[i] = m[num];
-            num += 1;
-        }
-        return s;
-    };
-};
-
-
-// this is O(n^2) as it has selection sort
-class Solution {
-public:
-    string sortVowels(string s) 
-    {
-        for(int i = 0; i < s.size(); i++)
-        {
-            // if the char is not vowel then continue
-            if(s[i] != 'A' 
-            && s[i] != 'E' 
-            && s[i] != 'O'
-            && s[i] != 'I' 
-            && s[i] != 'U'
-            && s[i] != 'a' 
-            && s[i] != 'e' 
-            && s[i] != 'i'
-            && s[i] != 'o' 
-            && s[i] != 'u')
-                continue;
-
-            // assign the ascii value of the char to smallest
-            int smallest = s[i];
-            int small = i;
-            int num;
-
-            // selection sort (O(n2))
-            for(int j = i; j < s.size(); j++)
+            while(true)
             {
-                if(s[j] != 'A' 
-                && s[j] != 'E' 
-                && s[j] != 'O'
-                && s[j] != 'I' 
-                && s[j] != 'U'
-                && s[j] != 'a' 
-                && s[j] != 'e' 
-                && s[j] != 'i'
-                && s[j] != 'o' 
-                && s[j] != 'u')
-                    continue;
-                num = s[j];
-                if(num <= smallest)
+                if(index < 10) 
                 {
-                    smallest = num;
-                    small = j;
+                    digit += index;
+                    break;
+                }
+                else
+                {
+                    // get the digit and also reduce the number
+                    digit += index % 10;
+                    index = index /10;
                 }
             }
-            // swap the smallest ascii value char with the currect char
-            swap(s[i], s[small]);
         }
-        return s;
-    };
+
+        // this as we need to find the absolute difference
+        long long diff; 
+        if(ele >= digit) diff = ele - digit;
+        if(ele < digit) diff = digit - ele;
+
+        // could use this too abs(ele - digit);
+        return diff;    
+    }
 };
