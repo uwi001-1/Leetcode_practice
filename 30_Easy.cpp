721bf767487d6c6692f84270748134a1c2eabb77
Kripa Hayanju <kripahayanju@gmail.com>
2026-06-19 12:53:43 -0500

    81

--- 30_Easy.cpp
+++ 30_Easy.cpp
@@ -1,25 +1,22 @@
 #include<iostream>
 using namespace std;
 
-// 189. Rotate Array
+// 1732. Find the Highest Altitude
 
 /***************
-Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+
+You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
 
 Example 1:
-Input: nums = [1,2,3,4,5,6,7], k = 3
-Output: [5,6,7,1,2,3,4]
-    Explanation:
-    rotate 1 steps to the right: [7,1,2,3,4,5,6]
-    rotate 2 steps to the right: [6,7,1,2,3,4,5]
-    rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Input: gain = [-5,1,5,0,-7]
+Output: 1
+    Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
 
 Example 2:
-Input: nums = [-1,-100,3,99], k = 2
-Output: [3,99,-1,-100]
-    Explanation: 
-    rotate 1 steps to the right: [99,-1,-100,3]
-    rotate 2 steps to the right: [3,99,-1,-100]
+Input: gain = [-4,-3,-2,-1,4,3,2]
+Output: 0
+    Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
 ***************/
 
 
@@ -55,30 +52,3 @@ public:
         nums = arr;
     }
 };
-
-
-// doesn't work 
-// Time Limit Exceeded
-/**************** 
-class Solution {
-public:
-    void rotate(vector<int>& nums, int k) 
-    {
-        int loop = 0;
-        while(loop != k)
-        {
-            vector<int> arr;
-            arr.push_back(nums[nums.size()-1]);
-            
-            loop += 1;
-
-            for(int i = 0; i<nums.size()-1; i++)
-            {
-                arr.push_back(nums[i]);
-            }
-
-            nums = arr;
-        }
-    }
-};
-***************/
