5fb9f39a171212760b62795a1aff75083e174886
Kripa Hayanju <kripahayanju@gmail.com>
2026-06-19 00:15:59 -0500

    79

--- 29_Medium.cpp
+++ 29_Medium.cpp
@@ -4,18 +4,57 @@ using namespace std;
 // 189. Rotate Array
 
 /***************
-Given an array nums of size n, return the majority element.
-
-The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 
 Example 1:
-Input: nums = [3,2,3]
-Output: 3
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+    Explanation:
+    rotate 1 steps to the right: [7,1,2,3,4,5,6]
+    rotate 2 steps to the right: [6,7,1,2,3,4,5]
+    rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
 Example 2:
-Input: nums = [2,2,1,1,1,2,2]
-Output: 2
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+    Explanation: 
+    rotate 1 steps to the right: [99,-1,-100,3]
+    rotate 2 steps to the right: [3,99,-1,-100]
 ***************/
+
+
+class Solution {
+public:
+    // array(nums) here is passed by reference
+    void rotate(vector<int>& nums, int k) 
+    {
+        // to handle k > nums.size() cases
+        k = k % nums.size();
+
+        // to loop 
+        int loop = nums.size() - k;
+        
+        // new array to push in elements in the order
+        vector<int> arr;
+        
+        while(loop != nums.size())
+        {
+            // add values from nums.size()-k
+            arr.push_back(nums[loop]);
+            
+            loop += 1;
+        }
+
+        // loop the rest of the array
+        for(int i = 0; i<nums.size()-k; i++)
+        {
+            arr.push_back(nums[i]);
+        }
+
+        // as we are returning the nums array as it is passed by reference
+        nums = arr;
+    }
+};
 
 
 // doesn't work 
