569f78dee9fb6a258b7ffbf102cb9b8365a952fd
Kripa Hayanju <kripahayanju@gmail.com>
2026-06-24 18:26:00 -0500

    106

--- 39_Medium.cpp
+++ 39_Medium.cpp
@@ -34,39 +34,143 @@ struct ListNode {
 
 class Solution {
 public:
-    vector<int> leftRightDifference(vector<int>& nums) 
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
     {
-        // 1st handle left Sum
-        vector<int> leftSum;
-        leftSum.push_back(0);
+       // save the head before advancing
+        ListNode* pHead = l1; 
+       
+        ListNode* curr1 = l1;
+        ListNode* prev = nullptr;
+        ListNode* curr2 = l2;
 
-        for(int i = 0; i < nums.size() - 1; i++)
+        int extra = 0;
+        int value;
+
+        while(true)
         {
-            leftSum.push_back(leftSum[i] + nums[i]);
+            // when the linked list has value
+            if(curr1 != nullptr && curr2 != nullptr)
+            {
+                value = curr1->val + curr2->val + extra;
+            
+                if(value > 9)
+                {
+                    // for the carry over ahead
+                    extra = value / 10;
+
+                    // take the last digit as the value
+                    value = value % 10;
+                }
+                else extra = 0;
+            
+                curr1->val = value;
+
+                prev = curr1;
+                curr1 = curr1->next;
+                curr2 = curr2-> next;
+            }
+
+            // when linked list 2 is done
+            else if(curr1 != nullptr)
+            {
+                value = curr1->val + extra;
+            
+                if(value > 9)
+                {
+                    extra = value / 10;
+                    value = value % 10;
+                }
+                else extra = 0;
+            
+                curr1->val = value;
+
+                prev = curr1;
+                curr1 = curr1->next;
+            }
+
+            // when linked list 1 is done
+            else if(curr2 != nullptr)
+            {
+                if(extra != 0)
+                {
+                    value = curr2->val + extra;
+            
+                    if(value > 9)
+                    {
+                        extra = value / 10;
+                        value = value % 10;
+                    }
+                    else extra = 0;
+                }
+                else value = curr2->val;
+
+                // add the new value to the end of the linked list 1
+                ListNode* pNew = new ListNode(value);
+
+                prev->next = pNew;
+                prev = pNew;
+                curr2 = curr2-> next;
+            }
+
+            // when both linked list is done but there still remains extra 
+            else if(extra != 0)
+            {
+                ListNode* pNew = new ListNode(extra);
+                prev->next = pNew;
+
+                break;
+            }
+            
+            else break;
         }
-
-        // 2nd handle right sum
-        vector<int> rightSum;
-        rightSum.push_back(0);
-        
-        int loop = 0;
-        for(int j = nums.size() - 1; j > 0; j--)
-        {
-            rightSum.push_back(rightSum[loop] + nums[j]);
-            loop += 1;
-        }
-
-        // then reverse the rightSum array
-        std::reverse(rightSum.begin(), rightSum.end());
-
-        // 3rd make the left and right difference in an array
-        vector<int> arr;
-        for(int k = 0; k < nums.size(); k++)
-        {
-            // make sure the array element is an absolute number
-            arr.push_back(abs(leftSum[k] - rightSum[k]));
-        }
-
-        return arr;
+        // return the head node pointing to the linked list 1
+        return pHead;
     }
 };
+
+
+// this didn't work out as the numbers could be big(can't fit in long long or int)
+// we also can't loop around the int value like an array
+/*******
+        ListNode* curr1 = l1;
+        ListNode* curr2 = l2;
+
+        int num = 0;
+        int n1 = 0, n2 = 0;
+        while(curr1 != nullptr)
+        {
+            if(curr1 != nullptr)
+            {
+                n1 += (curr1->val * (pow(10, num)));
+                curr1 = curr1->next;
+            }
+            num += 1;
+        }
+
+        int number = 0;
+        while(curr2 != nullptr)
+        {
+            if(curr2 != nullptr) 
+            {
+                n2 += (curr2->val * (pow(10, num)));
+                curr2 = curr2->next;
+            }
+            number += 1;
+        }
+
+        int sum = n1 + n2;
+
+        ListNode* pHead = nullptr;
+        for(int i =0; i < sum.size(); i++)
+        {
+            ListNode* pNew = new ListNode(sum[i])
+
+            if(pHead == nullptr) pHead = pNew;
+            else
+            {
+                pNew->next = pHead;
+                pHead = pNew;
+            }
+        }
+        return pHead;
+********/
