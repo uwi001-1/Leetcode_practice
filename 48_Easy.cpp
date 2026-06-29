3e2076ed4d0da2dfdad781df5bceb98c133b0daf
Kripa Hayanju <kripahayanju@gmail.com>
2026-06-29 15:07:00 -0500

    133

--- 48_Easy.cpp
+++ 48_Easy.cpp
@@ -1,26 +1,24 @@
 #include<iostream>
 using namespace std;
 
-// 231. Power of Two
+// 342. Power of Four
 
 /***************
-Given an integer n, return true if it is a power of two. Otherwise, return false.
+Given an integer n, return true if it is a power of four. Otherwise, return false.
 
-An integer n is a power of two, if there exists an integer x such that n == 2x.
+An integer n is a power of four, if there exists an integer x such that n == 4x.
 
 Example 1:
-Input: n = 1
-Output: true
-    Explanation: 20 = 1
-
-Example 2:
 Input: n = 16
 Output: true
-    Explanation: 24 = 16
+
+Example 2:
+Input: n = 5
+Output: false
 
 Example 3:
-Input: n = 3
-Output: false
+Input: n = 1
+Output: true
 ***************/
 
 
@@ -29,17 +27,17 @@ Output: false
 
 class Solution {
 public:
-    bool isPowerOfTwo(int n) 
+    bool isPowerOfFour(int n)
     {
         if(n == 1) return true;
 
-        // the power of 2 cannot be negative at all
+        // the power of 4 cannot be negative at all
         if(n <= 0) return false;
 
         // loop from 1 to 31 , as that's the max value in the constraints
         for(int i = 1; i < 32; i++)
         {
-            if(n == pow(2,i)) return true;
+            if(n == pow(4,i)) return true;
         }
 
         // if never true return false
--- README.md
+++ README.md
@@ -15,10 +15,10 @@ Started: March 2026
 
 | Difficulty | Solved |
 |------------|---------|
-| Easy       | 22 |
+| Easy       | 23 |
 | Medium     | 23 |
 | Hard       | 2 |
-| Total      | 47 |
+| Total      | 48 |
 
 
 ## Topics Covered
