a78108ffbbb3148fdecc9a46602c6ecb6f19bc85
Kripa Hayanju <kripahayanju@gmail.com>
2026-07-07 15:02:59 -0500

    178

--- 64_Easy.cpp
+++ 64_Easy.cpp
@@ -1,24 +1,22 @@
 #include<iostream>
 using namespace std;
 
-// 328. Odd Even Linked List
+// 206. Reverse Linked List
 
 /***************
-Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
-
-The first node is considered odd, and the second node is even, and so on.
-
-Note that the relative order inside both the even and odd groups should remain as it was in the input.
-
-You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+Given the head of a singly linked list, reverse the list, and return the reversed list.
 
 Example 1:
 Input: head = [1,2,3,4,5]
-Output: [1,3,5,2,4]
+Output: [5,4,3,2,1]
 
 Example 2:
-Input: head = [2,1,3,5,6,4,7]
-Output: [2,3,6,7,1,5,4]
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
 ***************/
 
 
--- README.md
+++ README.md
@@ -15,10 +15,10 @@ Started: March 2026
 
 | Difficulty | Solved |
 |------------|---------|
-| Easy       | 31 |
+| Easy       | 32 |
 | Medium     | 30 |
 | Hard       | 2 |
-| Total      | 63 |
+| Total      | 64 |
 
 
 ## Topics Covered
