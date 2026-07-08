038f895d8b47c82d22d72ac755369b8c37ac5b6b
Kripa Hayanju <kripahayanju@gmail.com>
2026-07-08 13:20:07 -0500

    183

--- 65_Medium.cpp
+++ 65_Medium.cpp
@@ -4,29 +4,106 @@ using namespace std;
 // 3756. Concatenate Non-Zero Digits and Multiply by Sum II
 
 /***************
-You are given an integer n.
+You are given a string s of length m consisting of digits. You are also given a 2D integer array queries, where queries[i] = [li, ri].
 
-Form a new integer x by concatenating all the non-zero digits of n in their original order. If there are no non-zero digits, x = 0.
+For each queries[i], extract the substring s[li..ri]. Then, perform the following:
 
-Let sum be the sum of digits in x.
+Form a new integer x by concatenating all the non-zero digits from the substring in their original order. If there are no non-zero digits, x = 0.
+Let sum be the sum of digits in x. The answer is x * sum.
+Return an array of integers answer where answer[i] is the answer to the ith query.
 
-Return an integer representing the value of x * sum.
+Since the answers may be very large, return them modulo 109 + 7.
 
 Example 1:
-Input: n = 10203004
-Output: 12340
+Input: s = "10203004", queries = [[0,7],[1,3],[4,6]]
+Output: [12340, 4, 9]
     Explanation:
-    The non-zero digits are 1, 2, 3, and 4. Thus, x = 1234.
-    The sum of digits is sum = 1 + 2 + 3 + 4 = 10.
-    Therefore, the answer is x * sum = 1234 * 10 = 12340.
+s[0..7] = "10203004"
+x = 1234
+sum = 1 + 2 + 3 + 4 = 10
+Therefore, answer is 1234 * 10 = 12340.
+s[1..3] = "020"
+x = 2
+sum = 2
+Therefore, the answer is 2 * 2 = 4.
+s[4..6] = "300"
+x = 3
+sum = 3
+Therefore, the answer is 3 * 3 = 9.
 
 Example 2:
-Input: n = 1000
-Output: 1
+Input: s = "1000", queries = [[0,3],[1,1]]
+Output: [1, 0]
     Explanation:
-    The non-zero digit is 1, so x = 1 and sum = 1.
-    Therefore, the answer is x * sum = 1 * 1 = 1.
+s[0..3] = "1000"
+x = 1
+sum = 1
+Therefore, the answer is 1 * 1 = 1.
+s[1..1] = "0"
+x = 0
+sum = 0
+Therefore, the answer is 0 * 0 = 0.
+
+Example 3:
+Input: s = "9876543210", queries = [[0,9]]
+Output: [444444137]
+    Explanation:
+s[0..9] = "9876543210"
+x = 987654321
+sum = 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1 = 45
+Therefore, the answer is 987654321 * 45 = 44444444445.
+We return 44444444445 modulo (109 + 7) = 444444137.
 ***************/
+
+
+class Solution {
+public:
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) 
+    {
+        const long long MOD = 1e9 + 7;
+        int m = s.size();
+
+        // prefix arrays
+        vector<long long> prefixSum(m+1, 0);    // sum of non-zero digits
+        vector<long long> prefixX(m+1, 0);      // concatenated non-zero number
+        vector<long long> prefixCount(m+1, 0);  // count of non-zero digits
+        vector<long long> pow10(m+1, 1);        // powers of 10
+
+        for(int i = 0; i < m; i++)
+        {
+            int digit = s[i] - '0';
+            prefixSum[i+1] = prefixSum[i] + digit;
+            prefixCount[i+1] = prefixCount[i] + (digit != 0 ? 1 : 0);
+            // only append non-zero digits to x
+            if(digit != 0)
+                prefixX[i+1] = (prefixX[i] * 10 + digit) % MOD;
+            else
+                prefixX[i+1] = prefixX[i];
+            pow10[i+1] = pow10[i] * 10 % MOD;
+        }
+
+        vector<int> arr;
+
+        for(auto& q : queries)
+        {
+            int l = q[0], r = q[1];
+
+            // sum of non-zero digits in [l,r]
+            long long sum = prefixSum[r+1] - prefixSum[l];
+
+            // count of non-zero digits in [l,r]
+            long long cnt = prefixCount[r+1] - prefixCount[l];
+
+            // x = prefixX[r+1] - prefixX[l] * 10^cnt
+            long long x = (prefixX[r+1] - prefixX[l] * pow10[cnt] % MOD + MOD) % MOD;
+
+            long long ans = (x * (sum % MOD)) % MOD;
+            arr.push_back((int)ans);
+        }
+
+        return arr;
+    }
+};
 
 
 // Time Limit Exceeded 
