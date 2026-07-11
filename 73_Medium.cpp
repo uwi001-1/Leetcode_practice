c2c7f82d21a6794de2645528cbf657d07262fb24
Kripa Hayanju <kripahayanju@gmail.com>
2026-07-11 14:52:45 -0500

    203

---
+++ 73_Medium.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+// 3751. Total Waviness of Numbers in Range I
+
+/***************
+You are given two integers num1 and num2 representing an inclusive range [num1, num2].
+
+The waviness of a number is defined as the total count of its peaks and valleys:
+
+- A digit is a peak if it is strictly greater than both of its immediate neighbors.
+- A digit is a valley if it is strictly less than both of its immediate neighbors.
+- The first and last digits of a number cannot be peaks or valleys.
+- Any number with fewer than 3 digits has a waviness of 0.
+
+Return the total sum of waviness for all numbers in the range [num1, num2].
+ 
+Example 1:
+Input: num1 = 120, num2 = 130
+Output: 3
+    Explanation:
+In the range [120, 130]:
+120: middle digit 2 is a peak, waviness = 1.
+121: middle digit 2 is a peak, waviness = 1.
+130: middle digit 3 is a peak, waviness = 1.
+All other numbers in the range have a waviness of 0.
+Thus, total waviness is 1 + 1 + 1 = 3.
+
+Example 2:
+Input: num1 = 198, num2 = 202
+Output: 3
+    Explanation:
+In the range [198, 202]:
+198: middle digit 9 is a peak, waviness = 1.
+201: middle digit 0 is a valley, waviness = 1.
+202: middle digit 0 is a valley, waviness = 1.
+All other numbers in the range have a waviness of 0.
+Thus, total waviness is 1 + 1 + 1 = 3.
+
+Example 3:
+Input: num1 = 4848, num2 = 4848
+Output: 2
+    Explanation:
+Number 4848: the second digit 8 is a peak, and the third digit 4 is a valley, giving a waviness of 2.
+***************/
+
+
+class Solution {
+public:
+    int totalWaviness(int num1, int num2) 
+    {
+        // use brute force
+        int count = 0;
+
+        for(int i = num1; i <= num2; i++)
+        {
+            // change it into a string so we can loop it
+            string s = to_string(i);
+
+            // base case; if size is less than 3 cannot have waviness
+            if(s.size() < 3) continue;
+
+            for(int j = 1; j < s.size()-1; j++)
+            {
+                // peak
+                if(s[j] > s[j-1] && s[j] > s[j+1]) count += 1;
+
+                // value
+                else if(s[j] < s[j-1] && s[j] < s[j+1]) count += 1;
+                
+                else continue;
+            }
+        }
+
+        return count;
+    }
+};
