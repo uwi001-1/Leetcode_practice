eee1ff3fc03e04deb5c8b4e1d5ffd827a7825bdf
Kripa Hayanju <kripahayanju@gmail.com>
2026-07-16 13:38:20 -0500

    233

--- 82_Medium.cpp
+++ 82_Medium.cpp
@@ -52,26 +52,48 @@ Form pairs: gcd(2, 8) = 2 and gcd(3, 6) = 3. Thus, the sum is 2 + 3 = 5.
 
 class Solution {
 public:
-    int gcdOfOddEvenSums(int n) 
+    int gcd(int a, int b)
     {
-        // as the calc is same as the num
-        return n;
+        while(b) { a %= b; swap(a,b); }
+        return a;
+    }    
+
+    long long gcdSum(vector<int>& nums) 
+    {
+        long long max = 0;
+        vector<int> prefixGcd;
+
+        for(int i =0; i < nums.size(); i++)
+        {
+            // change the max value to the new value if it's higher
+            if(max <= nums[i]) max = nums[i];
+
+            prefixGcd.push_back(gcd(nums[i], max));
+        } 
+        // now we are done with the prefixGcd array
+
+        // next we sort the array
+        std::sort(prefixGcd.begin(), prefixGcd.end());
+
+        int front = 0;
+        int back = prefixGcd.size() -1;
+
+        long long sum = 0;
+
+        while(true)
+        {
+            // avoid unpaired            
+            if(front == back || front > back) break;
+
+            // sum the gcd of the pairs 
+            sum += gcd(prefixGcd[front], prefixGcd[back]);
+
+            front++;
+            back--;
+        }
+
+        // return the sum of the gcd pairs of the prefixGcd array
+        return sum;
     }
 };
 
-/********************* 
-#include<numeric> 
-
-class Solution {
-public:
-    int gcdOfOddEvenSums(int n) 
-    {
-        int odd = n*n;
-        int even = n*(n+1);  
-
-        int result = std::gcd(odd, even);
-
-        return result;
-    }
-};
-*********************/
