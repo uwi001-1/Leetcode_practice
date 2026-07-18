9faad2e22c125a3e5ebcfbadc90c5739d029f9c4
Kripa Hayanju <kripahayanju@gmail.com>
2026-07-17 21:09:37 -0500

    242

--- 85_Hard.cpp
+++ 85_Hard.cpp
@@ -37,23 +37,100 @@ gcdPairs = [2].
 
 class Solution {
 public:
-    int addDigits(int num) 
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) 
     {
-        // if one digit then return it
-        if(num < 10) return num;
+        int maxVal = *max_element(nums.begin(), nums.end());
+        
+        // count frequency of each number
+        vector<long long> freq(maxVal + 1, 0);
+        for(int x : nums) freq[x]++;
 
-        while(num >= 10)
+        // cnt[d] = how many numbers in nums are divisible by d
+        vector<long long> cnt(maxVal + 1, 0);
+        for(int d = 1; d <= maxVal; d++)
         {
-            int sum = 0;
-
-            while(num > 0)
+            for(int multiple = d; multiple <= maxVal; multiple += d)
             {
-                int last = num % 10;
-                sum += last;
-                num = num / 10;
+                cnt[d] += freq[multiple];
             }
-            num = sum;
         }
-        return num;
+
+        // pairsWithGcdExactlyD[d] = pairs where GCD is EXACTLY d
+        // using inclusion-exclusion: subtract pairs where GCD is multiple of d
+        vector<long long> exact(maxVal + 1, 0);
+        for(int d = maxVal; d >= 1; d--)
+        {
+            // pairs where d divides both = cnt[d] choose 2
+            exact[d] = cnt[d] * (cnt[d] - 1) / 2;
+            
+            // subtract pairs where GCD is a multiple of d (2d, 3d, ...)
+            for(int multiple = 2 * d; multiple <= maxVal; multiple += d)
+            {
+                exact[d] -= exact[multiple];
+            }
+        }
+
+        // build prefix sum so we can answer queries in O(1)
+        vector<long long> prefix(maxVal + 2, 0);
+        for(int d = 1; d <= maxVal; d++)
+        {
+            prefix[d] = prefix[d-1] + exact[d];
+        }
+
+        // answer each query using binary search on prefix
+        vector<int> arr;
+        for(long long q : queries)
+        {
+            // find smallest d where prefix[d] > q
+            int lo = 1, hi = maxVal, ans = maxVal;
+            while(lo <= hi)
+            {
+                int mid = (lo + hi) / 2;
+                if(prefix[mid] > q)
+                {
+                    ans = mid;
+                    hi = mid - 1;
+                }
+                else lo = mid + 1;
+            }
+            arr.push_back(ans);
+        }
+
+        return arr;
     }
 };
+
+
+// Time Limit Exceeded (713/721)
+// this uses brute force
+/***************
+class Solution {
+public:
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) 
+    {
+        vector<int> gcdPairs;
+
+        // handle gcdPairs array
+        for(int i = 0; i < nums.size() - 1; i++)
+        {
+            for(int j = i + 1; j < nums.size(); j++)
+            {
+                gcdPairs.push_back(std::gcd(nums[i], nums[j]));
+            }
+        } 
+
+        // sort gcdPairs
+        std::sort(gcdPairs.begin(), gcdPairs.end());
+
+        vector<int> arr; 
+
+        // handle arr
+        for(int k = 0; k < queries.size(); k++)
+        {
+            arr.push_back(gcdPairs[queries[k]]);
+        }
+
+        return arr;   
+    }
+};
+***************/
