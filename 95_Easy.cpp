82044641e3353b7a26095ac5a56679e57f2494fd
Kripa Hayanju <kripahayanju@gmail.com>
2026-07-23 16:24:11 -0500

    271

--- 95_Easy.cpp
+++ 95_Easy.cpp
@@ -1,22 +1,33 @@
 #include<iostream>
 using namespace std;
 
-// 75. Sort Colors
+// 3731. Find Missing Elements
 
 /***************
-Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+You are given an integer array nums consisting of unique integers.
 
-We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+Originally, nums contained every integer within a certain range. However, some integers might have gone missing from the array.
+The smallest and largest integers of the original range are still present in nums.
 
-You must solve this problem without using the library's sort function.
+Return a sorted list of all the missing integers in this range. If no integers are missing, return an empty list.
 
 Example 1:
-Input: nums = [2,0,2,1,1,0]
-Output: [0,0,1,1,2,2]
+Input: nums = [1,4,2,5]
+Output: [3]
+Explanation:
+The smallest integer is 1 and the largest is 5, so the full range should be [1,2,3,4,5]. Among these, only 3 is missing.
 
 Example 2:
-Input: nums = [2,0,1]
-Output: [0,1,2]
+Input: nums = [7,8,6,9]
+Output: []
+Explanation:
+The smallest integer is 6 and the largest is 9, so the full range is [6,7,8,9]. All integers are already present, so no integer is missing.
+
+Example 3:
+Input: nums = [5,1]
+Output: [2,3,4]
+Explanation:
+The smallest integer is 1 and the largest is 5, so the full range should be [1,2,3,4,5]. The missing integers are 2, 3, and 4.
 ***************/
 
 
--- README.md
+++ README.md
@@ -15,10 +15,10 @@ Started: March 2026
 
 | Difficulty | Solved |
 |------------|---------|
-| Easy       | 49 |
+| Easy       | 50 |
 | Medium     | 41 |
 | Hard       | 4 |
-| Total      | 94 |
+| Total      | 95 |
 
 
 ## Topics Covered
