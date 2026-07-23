be95867744c0892425580c90bb22d2d6497bace6
Kripa Hayanju <kripahayanju@gmail.com>
2026-07-23 16:39:32 -0500

    277

--- 97_Easy.cpp
+++ 97_Easy.cpp
@@ -1,40 +1,27 @@
 #include<iostream>
 using namespace std;
 
-// 1051. Height Checker
+// 2706. Buy Two Chocolates
 
 /***************
-A school is trying to take an annual photo of all the students. 
-The students are asked to stand in a single file line in non-decreasing order by height. 
-Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+You are given an integer array prices representing the prices of various chocolates in a store. 
+You are also given a single integer money, which represents your initial amount of money.
 
-You are given an integer array heights representing the current order that the students are standing in. 
-Each heights[i] is the height of the ith student in line (0-indexed).
-Return the number of indices where heights[i] != expected[i].
+You must buy exactly two chocolates in such a way that you still have some non-negative leftover money. 
+You would like to minimize the sum of the prices of the two chocolates you buy.
+
+Return the amount of money you will have leftover after buying the two chocolates. 
+If there is no way for you to buy two chocolates without ending up in debt, return money. Note that the leftover must be non-negative. 
 
 Example 1:
-Input: heights = [1,1,4,2,1,3]
-Output: 3
-Explanation: 
-heights:  [1,1,4,2,1,3]
-expected: [1,1,1,2,3,4]
-Indices 2, 4, and 5 do not match.
+Input: prices = [1,2,2], money = 3
+Output: 0
+Explanation: Purchase the chocolates priced at 1 and 2 units respectively. You will have 3 - 3 = 0 units of money afterwards. Thus, we return 0.
 
 Example 2:
-Input: heights = [5,1,2,3,4]
-Output: 5
-Explanation:
-heights:  [5,1,2,3,4]
-expected: [1,2,3,4,5]
-All indices do not match.
-
-Example 3:
-Input: heights = [1,2,3,4,5]
-Output: 0
-Explanation:
-heights:  [1,2,3,4,5]
-expected: [1,2,3,4,5]
-All indices match
+Input: prices = [3,2,3], money = 3
+Output: 3
+Explanation: You cannot buy 2 chocolates without going in debt, so we return 3.
 ***************/
 
 
--- README.md
+++ README.md
@@ -15,10 +15,10 @@ Started: March 2026
 
 | Difficulty | Solved |
 |------------|---------|
-| Easy       | 51 |
+| Easy       | 52 |
 | Medium     | 41 |
 | Hard       | 4 |
-| Total      | 96 |
+| Total      | 97 |
 
 
 ## Topics Covered
