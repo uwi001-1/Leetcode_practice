75a923cebd250216c6ff2bd802b91c4bec934679
Kripa Hayanju <kripahayanju@gmail.com>
2026-07-23 16:18:12 -0500

    270

---
+++ 95_Easy.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+// 75. Sort Colors
+
+/***************
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+Input: nums = [2,0,1]
+Output: [0,1,2]
+***************/
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) 
+    {
+        // Could easily do this too
+        // std::sort(nums.begin(), nums.end()); 
+
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] == 0) count0++;
+            if(nums[i] == 1) count1++;
+            if(nums[i] == 2) count2++;
+        }
+
+        vector<int> arr;
+        
+        while(count0 > 0) 
+        {
+            arr.push_back(0);
+            count0--;
+        }
+        while(count1 > 0)
+        {
+            arr.push_back(1);
+            count1--;
+        }
+        while(count2 > 0)
+        {
+            arr.push_back(2);
+            count2--;
+        }
+
+        nums = arr; 
+    }
+};
