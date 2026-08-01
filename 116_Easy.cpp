ebd8c8e2fcea0c00ab5a19aa46b7b51dcba2b08f
Kripa Hayanju <kripahayanju@gmail.com>
2026-08-01 12:34:55 -0500

    334

--- 116_Easy.cpp
+++ 116_Easy.cpp
@@ -1,30 +1,22 @@
 #include<iostream>
 using namespace std;
 
-// 3222. Find the Winning Player in Coin Game
+// 345. Reverse Vowels of a String
 
 /***************
-You are given two positive integers x and y, denoting the number of coins with values 75 and 10 respectively.
+Given a string s, reverse only all the vowels in the string and return it.
 
-Alice and Bob are playing a game. Each turn, starting with Alice, 
-the player must pick up coins with a total value 115. If the player is unable to do so, they lose the game.
-
-Return the name of the player who wins the game if both players play optimally.
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
 
 Example 1:
-Input: x = 2, y = 7
-Output: "Alice"
+Input: s = "IceCreAm"
+Output: "AceCreIm"
 Explanation:
-The game ends in a single turn:
-Alice picks 1 coin with a value of 75 and 4 coins with a value of 10.
+The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
 
 Example 2:
-Input: x = 4, y = 11
-Output: "Bob"
-Explanation:
-The game ends in 2 turns:
-Alice picks 1 coin with a value of 75 and 4 coins with a value of 10.
-Bob picks 1 coin with a value of 75 and 4 coins with a value of 10.
+Input: s = "leetcode"
+Output: "leotcede"
 ***************/
 
 
--- README.md
+++ README.md
@@ -15,10 +15,10 @@ Started: March 2026
 
 | Difficulty | Solved |
 |------------|---------|
-| Easy       | 62 |
+| Easy       | 63 |
 | Medium     | 48 |
 | Hard       | 5 |
-| Total      | 115 |
+| Total      | 116 |
 
 
 ## Topics Covered
