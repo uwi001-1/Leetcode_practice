d1f070b8cf5d0e119da6ea4de16159ac01b536dc
Kripa Hayanju <kripahayanju@gmail.com>
2026-08-01 12:34:04 -0500

    333

---
+++ 116_Easy.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+// 3222. Find the Winning Player in Coin Game
+
+/***************
+You are given two positive integers x and y, denoting the number of coins with values 75 and 10 respectively.
+
+Alice and Bob are playing a game. Each turn, starting with Alice, 
+the player must pick up coins with a total value 115. If the player is unable to do so, they lose the game.
+
+Return the name of the player who wins the game if both players play optimally.
+
+Example 1:
+Input: x = 2, y = 7
+Output: "Alice"
+Explanation:
+The game ends in a single turn:
+Alice picks 1 coin with a value of 75 and 4 coins with a value of 10.
+
+Example 2:
+Input: x = 4, y = 11
+Output: "Bob"
+Explanation:
+The game ends in 2 turns:
+Alice picks 1 coin with a value of 75 and 4 coins with a value of 10.
+Bob picks 1 coin with a value of 75 and 4 coins with a value of 10.
+***************/
+
+
+class Solution {
+public:
+    string winningPlayer(int x, int y) 
+    {
+        // base case - when Alice can't play at first 
+        if(y < 4) return "Bob";
+
+        bool Alice = false;
+        bool Bob = true;
+
+        while(x >= 1 && y >= 4)
+        {
+            // reduce it by needed 
+            x -= 1;
+            y -= 4;
+
+            if(Bob) 
+            {
+                Alice = true;
+                Bob = false;
+            }
+            else 
+            {
+                Bob = true;
+                Alice = false;
+            }
+        }
+
+        if(Bob) return "Bob";
+        return "Alice";   
+    }
+};
