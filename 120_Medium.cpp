1af4a60fa8b4f9c96c06fcf3eabdc16b5ad2a14e
Kripa Hayanju <kripahayanju@gmail.com>
2026-08-02 14:03:12 -0500

    345

---
+++ 120_Medium.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+// 877. Stone Game
+
+/***************
+Alice and Bob play a game with piles of stones. There are an even number of piles arranged in a row, and each pile has a positive integer number of stones piles[i].
+
+The objective of the game is to end with the most stones. The total number of stones across all the piles is odd, so there are no ties.
+
+Alice and Bob take turns, with Alice starting first. Each turn, a player takes the entire pile of stones either from the beginning or from the end of the row. 
+This continues until there are no more piles left, at which point the person with the most stones wins.
+
+Assuming Alice and Bob play optimally, return true if Alice wins the game, or false if Bob wins.
+
+Example 1:
+Input: piles = [5,3,4,5]
+Output: true
+Explanation: 
+Alice starts first, and can only take the first 5 or the last 5.
+Say she takes the first 5, so that the row becomes [3, 4, 5].
+If Bob takes 3, then the board is [4, 5], and Alice takes 5 to win with 10 points.
+If Bob takes the last 5, then the board is [3, 4], and Alice takes 4 to win with 9 points.
+This demonstrated that taking the first 5 was a winning move for Alice, so we return true.
+
+Example 2:
+Input: piles = [3,7,2,3]
+Output: true
+***************/
+
+
+class Solution {
+public:
+    // solve with recursion and dynamic programming
+    vector<vector<int>> dp;
+
+    int solve(vector<int>& nums, int l, int r)
+    {
+        // base case
+        if (l == r) return nums[l];
+
+        if (dp[l][r] != INT_MIN)
+            return dp[l][r];
+        
+        int takeLeft = nums[l] - solve(nums, l + 1, r);
+        int takeRight = nums[r] - solve(nums, l, r - 1);
+
+        return dp[l][r] = max(takeLeft, takeRight);
+    }
+
+    bool stoneGame(vector<int>& piles) 
+    {
+        int n = piles.size();
+        dp.assign(n, vector<int>(n, INT_MIN));
+
+        return solve(piles, 0, n - 1) >= 0;
+    }
+};
