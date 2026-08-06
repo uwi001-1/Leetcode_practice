00ed8c921b53a8ac8cd1c8e444c2f81d93dd8170
Kripa Hayanju <kripahayanju@gmail.com>
2026-08-06 12:45:11 -0500

    365

--- 126_Easy.cpp
+++ 126_Easy.cpp
@@ -23,64 +23,29 @@ The digit product of 16 is 6, which is divisible by 3, making it the smallest nu
 
 class Solution {
 public:
-    // DFS (Dept-First Search)
-    
-    void dfs(int node, vector<vector<int>>& graph, vector<bool>& suspicious)
+    int smallestNumber(int n, int t) 
     {
-        suspicious[node] = true;
+        int num = n;
 
-        for (int nei : graph[node])
+        while(true)
         {
-            if (!suspicious[nei])
+            int pro = 1;
+            int x = num; 
+
+            if (x == 0)
+                pro = 0;
+            else 
             {
-                dfs(nei, graph, suspicious);
-            }
-        }
-    }
-
-    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) 
-    {
-        // Step 1: Build adjacency list
-        vector<vector<int>> graph(n);
-
-        for (auto &edge : invocations)
-        {
-            graph[edge[0]].push_back(edge[1]);
-        }
-
-        // Step 2: Find all suspicious methods
-        vector<bool> suspicious(n, false);
-        dfs(k, graph, suspicious);
-
-        // Step 3: Check if any non-suspicious method calls a suspicious method
-        for (auto &edge : invocations)
-        {
-            int u = edge[0];
-            int v = edge[1];
-
-            if (!suspicious[u] && suspicious[v])
-            {
-                // Cannot remove anything
-                vector<int> ans;
-                for (int i = 0; i < n; i++)
+                while(x > 0)
                 {
-                    ans.push_back(i);
+                    pro *= x % 10;
+                    x = x / 10;
                 }
-                return ans;
             }
+
+            if(pro % t == 0) return num;
+
+            num++;
         }
-
-        // Step 4: Return all remaining (non-suspicious) methods
-        vector<int> ans;
-
-        for (int i = 0; i < n; i++)
-        {
-            if (!suspicious[i])
-            {
-                ans.push_back(i);
-            }
-        }
-
-        return ans;
     }
 };
